6b707e9bf672a6663d56dc8d6a704885b5d1ffcd
mouna khila <mouna.khila@aleriaconsulting.eu>
2021-12-27 13:33:37 +0100

    first commit for my first mobile app Top Quiz

---
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.topquiz;
+
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.CharArrayWriter;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mGreetingTextView;
+    EditText mNameInput;
+    Button mPlayButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mGreetingTextView=findViewById(R.id.activity_main_greeting_txt);
+        mNameInput=findViewById(R.id.activity_main_name_input);
+        mPlayButton=findViewById(R.id.activity_main_play_btn);
+        mPlayButton.setEnabled(false);
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mPlayButton.setEnabled(s.toString().length() != 0);
+
+            }
+        });
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+
+    }
+}
